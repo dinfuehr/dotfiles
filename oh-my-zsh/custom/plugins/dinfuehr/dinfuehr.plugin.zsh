bcbf9575d630e95905b215df35babc7fe012f592
Dominik Inführ <dominik.infuehr@gmail.com>
2013-04-02 22:42:58 +0200

    c, h als Abkürzung

--- oh-my-zsh/custom/plugins/dinfuehr/dinfuehr.plugin.zsh
+++ oh-my-zsh/custom/plugins/dinfuehr/dinfuehr.plugin.zsh
@@ -1,5 +1,14 @@
+c() { cd ~/code/$1; }
+_c() { _files -W ~/code -/; }
+compdef _c c
+
+h() { cd ~/$1; }
+_h() { _files -W ~/ -/; }
+compdef _h h
+
 alias casmi=~/projects/casmi/build/casmi
 alias casmintr=~/projects/casm2cpp/casmintr
+alias subl=sublime
 
 function server() {
   local port="${1:-8000}"
