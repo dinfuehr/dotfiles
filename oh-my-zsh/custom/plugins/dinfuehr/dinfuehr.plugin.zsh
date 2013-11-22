c636bc389aade60c329ad9850f9de33745a0a455
Dominik Inführ <dominik.infuehr@gmail.com>
2013-11-22 22:28:19 +0100

    casmi aliases entfernt

--- oh-my-zsh/custom/plugins/dinfuehr/dinfuehr.plugin.zsh
+++ oh-my-zsh/custom/plugins/dinfuehr/dinfuehr.plugin.zsh
@@ -6,26 +6,15 @@ h() { cd ~/$1; }
 _h() { _files -W ~/ -/; }
 compdef _h h
 
-alias casmi=~/code/casmi/build/casmi
-alias casmintr=~/code/casm2cpp/casmintr
-alias carma=~/Dropbox/tu-wien/bachelorarbeit/carma/carma
-
 if [[ $(uname) == Linux ]]; then
   alias pbcopy='xclip -selection clipboard'
   alias pbpaste='xclip -selection clipboard -o'
-  #alias subl=sublime
-else
-  #alias subl="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
 fi
 
 function server() {
   local port="${1:-8000}"
   open "http://localhost:${port}"
   python -m SimpleHTTPServer "$port"
-}
-
-function svl() {
-  ssh root@sv-langenrohr.at
 }
 
 function ffp() {
