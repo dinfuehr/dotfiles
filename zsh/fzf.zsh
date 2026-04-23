0ef16bda1796bb0dd396e877d588bae4b4012cfc
Dominik Inführ <dominik.infuehr@gmail.com>
2026-04-23 02:53:36 +0200

    Support older fzf versions

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,5 +1,13 @@
 if command -v fzf >/dev/null 2>&1; then
-  source <(fzf --zsh)
+  # fzf >=0.48 ships shell integration via `fzf --zsh`; older Debian
+  # packages install the scripts under /usr/share/doc/fzf/examples.
+  if [[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]]; then
+    source /usr/share/doc/fzf/examples/key-bindings.zsh
+    [[ -f /usr/share/doc/fzf/examples/completion.zsh ]] && \
+      source /usr/share/doc/fzf/examples/completion.zsh
+  else
+    source <(fzf --zsh)
+  fi
   # Ctrl-T collides with zellij; move file-widget to Alt-T.
   bindkey -r '^T'
   bindkey '^[t' fzf-file-widget
